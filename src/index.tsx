212b61bc911ad41f033eedb62709ca60f4a16c66
FT <htrevinomtz@gmail.com>
2021-10-12 15:08:24 -0500

    Update element to inject

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,12 @@ const rootElementClass = 'chess-com-4vi-root';
 const rootElement = document.createElement('div');
 rootElement.className = rootElementClass;
 
-const insertElement = document.querySelector('.new-game-index-main');
+let insertElement: Element | null = null;
+if (document.URL.includes('/play/online')) {
+  insertElement = document.querySelector('.new-game-index-main');  
+} else if (document.URL.includes('/play/computer')) {
+  insertElement = document.querySelector('.layout-player.layout-bottom-player');
+}
 
 if (insertElement) {
   insertElement.appendChild(rootElement);
